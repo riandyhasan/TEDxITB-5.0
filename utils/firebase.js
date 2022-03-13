860fdef2f7ca5e73c34a51594c0007a02ca20eb7
riandyhasan <riandyhsn@gmail.com>
2022-03-13 08:58:07 +0700

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyCcwRardb8MdnBAbgKL5fY1tFX5uIvq9Hk",
+  authDomain: "tedxitb-32001.firebaseapp.com",
+  databaseURL: "https://tedxitb-32001-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "tedxitb-32001",
+  storageBucket: "tedxitb-32001.appspot.com",
+  messagingSenderId: "587310095515",
+  appId: "1:587310095515:web:a07d04d5d60478be4fba3e",
 };
 
 const app = initializeApp(firebaseConfig);
