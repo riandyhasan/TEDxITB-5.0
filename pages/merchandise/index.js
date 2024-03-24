139268ad1160e002919397cdcf4c74526283bd2e
Riandy Hasan <71575420+riandyhasan@users.noreply.github.com>
2024-03-25 00:52:05 +0700

    feat: active merch

--- pages/merchandise/index.js
+++ pages/merchandise/index.js
@@ -1,21 +1,21 @@
 import Head from "next/head";
 import Layout from "../../components/navigation/Layout";
-// import Merch from "../../components/pages/merch/Merch";
+import Merch from "../../components/pages/merch/Merch";
 import Close from "../../components/pages/merch/Close";
-// import getMerch from "../../hooks/merch/merch";
-// import getCart from "../../hooks/cart/cart";
-// import Loading from "../../components/loading/Loading";
+import getMerch from "../../hooks/merch/merch";
+import getCart from "../../hooks/cart/cart";
+import Loading from "../../components/loading/Loading";
 
 export default function MerchandisePage() {
-  // const data = getMerch();
-  // const dataCart = getCart();
+  const data = getMerch();
+  const dataCart = getCart();
   return (
     <Layout>
       <Head>
         <title>TEDxITB 5.0 | Merchandise</title>
       </Head>
-      {/* <Merch data={data} dataCart={dataCart} /> */}
-      <Close />
+       <Merch data={data} dataCart={dataCart} /> 
+      {/*<Close />*/}
     </Layout>
   );
 }
